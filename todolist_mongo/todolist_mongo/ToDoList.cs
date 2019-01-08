d9923d68f40f6b79f02947e2528e80ccecd729dc
Nivethan <nivethan.sethupathi@gmail.com>
2019-01-08 17:51:16 -0600

    moving CRUD operations into ToDoList class.

---
+++ todolist_mongo/todolist_mongo/ToDoList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace todolist_mongo
+{
+    class ToDoList
+    {
+        // data
+        private MongoClient client;
+
+        // default constructor
+        public ToDoList()
+        {
+            // open connection
+            this.client = new MongoClient("mongodb://localhost:27017");
+
+            // create database
+            IMongoDatabase db = client.GetDatabase("independent");
+
+            // create collection
+            var collection = db.GetCollection<BsonDocument>("tasks");
+        }
+    }
+}
